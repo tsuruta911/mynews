0638992fc034ff254f69c908bcd1523a749febf1
鶴田 <crane911sho@icloud.com>
2021-08-04 14:43:42 +0000

    Laravel12 kadai

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -3,14 +3,14 @@
     
     
     {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-    @section('title','My プロフィール')
+    @section('title','ニュース作成')
     
     {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
     @section('content')
         <div class="container">
             <div class="row">
                 <div class="col-md-8 mx-auto">
-                    <h2>My プロフィール</h2>
+                    <h2>ニュース作成</h2>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::group(['prefix' => 'admin'], function() {
 
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
     
 
