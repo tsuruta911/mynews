e11bc1a66bb88ab01c6f757bcccbc518b80874b5
鶴田 <crane911sho@icloud.com>
2021-08-14 08:58:35 +0000

    Laravel 17

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\Controller;
 
 // 以下を追記することでNews Modelが扱えるようになる
 use App\News;
+//History Modelの使用を宣言
+use App\History;
+//Carbonを使って取得した現在時刻を、History Modelの edited_at として記録します。
+use Carbon\Carbon;
 
 class NewsController extends Controller
 {
@@ -91,8 +95,14 @@ class NewsController extends Controller
       unset($news_form['_token']);
       // 該当するデータを上書きして保存する
       $news->fill($news_form)->save();
+      
+      // 以下を追記
+        $history = new History;
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
 
-      return redirect('admin/news');
+      return redirect('admin/news/');
   }
   
   // 以下を追記　　
